dc5c4adc7c6598d5f61517168489ac618e176190
Awe Ayomidipupo <pupoawe@gmail.com>
2023-02-24 17:36:16 +0100

    feat: setup authentication middleware

---
+++ src/middlewares/authMiddleware.ts
@@ -0,0 +1,28 @@
+import { handleAsync, verifyJWT } from "../utils/helpers";
+import { Unauthorized } from "../errors/httpErrors";
+
+// Auth middleware
+export const auth = handleAsync((req, res, next) => {
+  // Get auth from header
+  const { authorization } = req.headers;
+
+  // Missing auth header
+  if (!authorization) throw new Unauthorized("Missing Authorization Header");
+
+  // Token is a bearer token format i.e `Bearer <token>`
+  const token = authorization.split(" ")[1];
+
+  // Malformed auth token
+  if (!token) throw new Unauthorized("Malformed Token");
+
+  // payload is defined when error is null and vice versa
+  const { payload } = verifyJWT(token);
+
+  // Invalid or expired token
+  if (!payload) throw new Unauthorized("Invalid or Expired Auth Token");
+
+  // Set user id in request object
+  req.user = payload.uid;
+
+  next();
+});
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,6 +1,10 @@
 import { NextFunction, Request, Response, RequestHandler } from "express";
 import { IUser } from "../models/user.model";
-import jwt, { SignOptions } from "jsonwebtoken";
+import jwt, { JwtPayload, SignOptions } from "jsonwebtoken";
+
+interface UserJwtPayload extends JwtPayload {
+  uid: string;
+}
 
 export function handleAsync(callback: RequestHandler) {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -27,4 +31,21 @@ export function generateLoginToken(user: IUser) {
   const token = jwt.sign(payload, process.env.SECRET as string, options);
 
   return token;
+}
+
+// Wrapper function for jwt.verify
+export function verifyJWT(token: string) {
+  let payload = null;
+  let error = null;
+
+  try {
+    // Verify and decode token
+    payload = jwt.verify(token, process.env.SECRET as string) as UserJwtPayload;
+  } catch (err: any) {
+    // An error is thrown if jwt is invalid or some other error occured
+    error = err;
+  }
+
+  // Either payload or error would be set while the other would be null
+  return { payload, error };
 }
