ba39406726a8cf42b0864192ab3d02d1fc4a2005
Awe Ayomidipupo <pupoawe@gmail.com>
2023-03-05 23:54:38 +0100

    feat: add filter to accept only video files

--- src/config/multer.config.ts
+++ src/config/multer.config.ts
@@ -2,6 +2,7 @@ import multerS3 from "multer-s3";
 import multer from "multer";
 import * as dotenv from "dotenv";
 import s3Client from "./aws.config";
+import { BadRequest } from "../errors/httpErrors";
 dotenv.config();
 
 const bucket = process.env.DO_BUCKET_NAME as string;
@@ -12,6 +13,14 @@ const upload = multer({
     bucket,
     s3: s3Client,
   }),
+  fileFilter: (req, file, cb) => {
+    const acceptedFileTypes = ["video/mp4", "video/x-matroska"];
+
+    if (!acceptedFileTypes.includes(file.mimetype))
+      return cb(new BadRequest("Invalid video type"));
+
+    cb(null, true);
+  },
 });
 
 export default upload;
